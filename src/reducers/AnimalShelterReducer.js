c49b65664e9dea1407f9aeba1a2bb896ffbd117d
wilsonng21 <wilsongch92@gmail.com>
2020-11-05 14:36:09 -0500

    adding animals updates shelter home and also shelter

--- src/reducers/AnimalShelterReducer.js
+++ src/reducers/AnimalShelterReducer.js
@@ -9,8 +9,22 @@ const animalShelterReducer = (state = initialState, action) => {
                 ...state,
                 animalShelters: [ ...state.animalShelters, ...action.payload ]
             }
-        // case 'ADD_ANIMAL':
-        //     console.log('hello from animalShelterReducer')
+        case 'ADD_ANIMAL':
+            let foundShelter = state.animalShelters.find(shelter => shelter.id === action.payload.animal_shelter.id)
+            let foundShelterIndex = state.animalShelters.findIndex(shelter => shelter.id === foundShelter.id)
+            let copyOfAnimals = [ ...foundShelter.animals, {
+                id: action.payload.id,
+                capitalized_name: action.payload.capitalized_name,
+                capitalized_species: action.payload.capitalized_species,
+                description: action.payload.description
+            }]
+            let copyOfShelters = state.animalShelters
+            foundShelter.animals = copyOfAnimals
+            copyOfShelters[foundShelterIndex] = foundShelter
+            return {
+                ...state,
+                animalShelters: [ ...copyOfShelters ]
+            }
         default:
             return state
     }
